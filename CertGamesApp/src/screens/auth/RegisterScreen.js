a4daa587aecd74649cb020d9b39f94f67628e748
Yoshi2003 <cartertechsolution@gmail.com>
2025-04-02 03:57:03 -0400

    minor changes

--- CertGamesApp/src/screens/auth/RegisterScreen.js
+++ CertGamesApp/src/screens/auth/RegisterScreen.js
@@ -473,7 +473,7 @@ const RegisterScreen = () => {
                     <Ionicons name="person-outline" size={20} color="#AAAAAA" style={styles.inputIcon} />
                     <TextInput
                       style={styles.input}
-                      placeholder="Unique Username"
+                      placeholder="Unique Username update"
                       placeholderTextColor="#AAAAAA"
                       value={username}
                       onChangeText={setUsername}

26cb352c1d90147c1f68fc526c4154b3ae884175
Carter Perez <cartertechsolution@gmail.com>
2025-04-11 00:09:40 -0400

    Update babel.config.js

--- CertGamesApp/babel.config.js
+++ CertGamesApp/babel.config.js
@@ -2,21 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   
   const plugins = [
-    // Any other plugins you might have
   ];
   
-  // Only add transform-remove-console in production builds
   if (process.env.NODE_ENV === 'production') {
     plugins.push(['transform-remove-console', { exclude: [] }]);
-    // Empty array means no console methods are excluded from removal
   }
   
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       ...plugins,
-      
-      // Reanimated plugin must be listed last
+
       'react-native-reanimated/plugin',
     ],
   };
